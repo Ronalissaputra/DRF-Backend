f25a3b623fc18b228b74b16980ab9fcd4682b775
Ronalissaputra <ronalissaputra@192.168.1.17>
2023-09-07 15:41:21 +0800

    create method GET POST PUT DELETE

--- backend/blog/urls.py
+++ backend/blog/urls.py
@@ -2,11 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("authors/", views.AuthorListCreateView.as_view(), name="author-list-create"),
-    path("blogs/", views.BlogListCreateView.as_view(), name="blog-list-create"),
-    path(
-        "blogs/author/<int:author_id>/",
-        views.BlogByauthorListView.as_view(),
-        name="blog-by-author",
-    ),
+    path("blogs/", views.Blog_list, name="blog-list"),
+    path("blog/<int:id>/", views.Blog_detail, name="blog-detail"),
 ]
--- backend/blog/views.py
+++ backend/blog/views.py
@@ -1,21 +1,47 @@
-from rest_framework import generics
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework.pagination import PageNumberPagination
 from .models import Author, Blog
 from .serializers import AuthorSerializer, BlogSerializer
 
 
-class AuthorListCreateView(generics.ListCreateAPIView):
-    queryset = Author.objects.all()
-    serializer_class = AuthorSerializer
+class BlogListPagination(PageNumberPagination):
+    page_size = 10
 
 
-class BlogListCreateView(generics.ListCreateAPIView):
-    queryset = Blog.objects.all()
-    serializer_class = BlogSerializer
+@api_view(["GET", "POST"])
+def Blog_list(request):
+    if request.method == "GET":
+        paginator = BlogListPagination()
+        blogs = Blog.objects.all()
+        result_page = paginator.paginate_queryset(blogs, request)
+        serializer = BlogSerializer(result_page, many=True)
+        return paginator.get_paginated_response(serializer.data)
+    elif request.method == "POST":
+        serializer = BlogSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-class BlogByauthorListView(generics.ListAPIView):
-    serializer_class = BlogSerializer
+@api_view(["GET", "PUT", "DELETE"])
+def Blog_detail(request, id):
+    try:
+        blogs = Blog.objects.get(id=id)
+    except Blog.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
-    def get_queryset(self):
-        author_id = self.kwargs["author_id"]
-        return Blog.objects.filter(author_id=author_id)
+    if request.method == "GET":
+        serializer = BlogSerializer(blogs)
+        return Response(serializer.data)
+    elif request.method == "PUT":
+        serializer = BlogSerializer(blogs, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == "DELETE":
+        blogs.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
